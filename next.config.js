fc5bb7364bdb2af4a049091516054517cf08d16d
Isaac Turner <spacey_sooty@outlook.com>
2023-05-24 08:53:13 +0800

    intial commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
-
-module.exports = nextConfig
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        unoptimized: true
+    },
+    trailingSlash: true
+};
+module.exports = nextConfig;
